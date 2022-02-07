3bdb5976cb012ef99d141de0a3a2f74aa73551f8
Fasten90 <fasten90@gmail.com>
2022-02-07 22:58:29 +0100

    Practice string printing

--- small-rust-app/src/main.rs
+++ small-rust-app/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
+
+    let string_var = String::from("This string");
+    let chars = string_var.chars();
+    
+    //print(string_var);  // Does not work
+    
+    //print!(string_var); // error: format argument must be a string literal
+    
+    print!("{}", string_var);
+
+    //print!("{}", chars); > // error[E0277]: `Chars<'_>` doesn't implement `std::fmt::Display`
+
+    println!("{}", chars.count());
+
+    println!("Bla!"); // OK
+
+
 }
